00ff3dc06d3866f65b9429d0698ebc36b3df0bfa
KWAVE\kwave <kwave55@gmail.com>
2017-07-04 13:08:06 +0900

    오전 수업 내용 업데이트 1. 외부 저장소 권한설정 2. 사진 firebase storage에 upload하기

--- app/src/main/java/com/kwave/android/firebasebbs/MainActivity.java
+++ app/src/main/java/com/kwave/android/firebasebbs/MainActivity.java
@@ -2,20 +2,40 @@ package com.kwave.android.firebasebbs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.kwave.android.firebasebbs.Util.PermissionControl;
+
+public class MainActivity extends AppCompatActivity implements PermissionControl.CallBack{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PermissionControl.checkVersion(this);           // 퍼미션 실시
      }
 
     public void startBbs(View view){
      Intent intent = new Intent(this, ListActivity.class);
      startActivity(intent);
    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionControl.onResult(this,requestCode,grantResults);
+    }
+
+    @Override
+    public void init() {
+        // 초기화 처리
+        Intent intent = new Intent(this, NaviActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
 }
+
---
+++ app/src/main/java/com/kwave/android/firebasebbs/domain/Bbs.java
@@ -0,0 +1,25 @@
+package com.kwave.android.firebasebbs.domain;
+
+/**
+ * Created by kwave on 2017-07-04.
+ */
+
+public class Bbs {
+    public String id;    // 파이어베이스의 push 로 자동생성된다.
+    public String title;
+    public String author;
+    public String content;
+    public String fileUriString;
+    public long date;
+    public long count; // 조회수
+
+    public Bbs(){
+
+    }
+
+    public Bbs(String title, String author, String content){
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
+}
